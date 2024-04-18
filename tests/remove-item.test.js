ead3231028075c8cf14e0bce3480f0a46d9e6015
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-18 11:58:47 +0100

    ADD: Remove item test added

---
+++ tests/remove-item.test.js
@@ -0,0 +1,15 @@
+import { Basket, Item } from "../index.js";
+
+const testRemoveItem = () => {
+  const basket = new Basket();
+  const item = new Item("chocolate", 1.49, 0);
+
+  basket.addItem(item);
+  basket.removeItem(0);
+  const newItems = basket.getItems();
+  const newBasketItemsLength = newItems.length;
+  return newBasketItemsLength === 0;
+}
+
+console.log("TEST: Items are successfully removed from basket.");
+testRemoveItem() ? console.log("PASS") : console.log("FAIL");

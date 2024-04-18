350a8585fbd149c3ef071e0ca6edc90d2dfa6305
Kenen <kenen1996@hotmail.co.uk>
2024-04-18 13:02:42 +0100

    edit test-1

--- tests/test-file-1.js
+++ tests/test-file-1.js
@@ -5,28 +5,28 @@ import { Basket, Item } from "../index.js"
 // Arrange
 let item1, item2, item3
 
-let expected 
 let actual, result
-
-
 
 const testBasket = new Basket()
 item1 = new Item('salmon', 5, 1)
+
 item2 = new Item('cheese', 3, 2)
-item3 = new Item('beef',4,3)
+item3 = new Item('beef', 4, 3)
+
+// let expected = new Item('salmon', 5, 1)
+let expected = item1
 
 // Act
 testBasket.addItem(item1)
 
-
 // Assert
 actual = item1
-expected = testBasket.getItems()[0]
+// expected = testBasket.getItems()[0]
+console.log(actual)
+console.log(expected)
 
 result = assertEquals(actual, expected)
 
-console.log(`Test result: ${result}`);
-
-
-
 // Report
+console.log(`Item added to basket:`);
+console.log(`Test result: ${result}`);

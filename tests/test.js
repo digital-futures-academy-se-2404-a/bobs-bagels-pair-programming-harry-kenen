021221abec71605ab04b0ec967f76b40ed504d88
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-18 16:33:55 +0100

    test: added test.js file

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,16 @@
-import testFullCapacity from "./full-capacity.test";
+import testFullCapacity from "./full-capacity.test.js";
+import testIncreaseCapacity from "./increase-capacity.test.js";
+import testRemoveItem from "./remove-item.test.js";
+import testRemoveItemEmpty from "./remove-item-empty.test.js";
+
+const tests = [
+  testFullCapacity,
+  testIncreaseCapacity,
+  testRemoveItem,
+  testRemoveItemEmpty
+];
+
+tests.forEach((test, i) => {
+  console.log(`=== TEST #${i + 1} ===`);
+  test();
+});

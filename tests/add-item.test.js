9e1dc911a5e8df81dc3f5a8e57e34ae84653ab1e
Kenen <kenen1996@hotmail.co.uk>
2024-04-18 16:46:52 +0100

    add array increase test to add-items

--- tests/add-item.test.js
+++ tests/add-item.test.js
@@ -3,31 +3,67 @@ import { assertEquals } from "./test-framework.js";
 import Basket from "../src/basket.js";
 import Item from "../src/item.js";
 
+// AFTER EACH Function
+const afterEach = () => {
+    expected = undefined;
+    actual = undefined;
+    result = undefined;
+    item1 = undefined;
+    testBasket.items = [];
+};
+
+//? TEST 1
+//* Test item is actually added to basket
+
 // Arrange
 let item1, item2, item3
-
 let actual, result
 
 const testBasket = new Basket()
 item1 = new Item('salmon', 5, 1)
 
-item2 = new Item('cheese', 3, 2)
-item3 = new Item('beef', 4, 3)
+//item2 = new Item('cheese', 3, 2)
+//item3 = new Item('beef', 4, 3)
 
-// let expected = new Item('salmon', 5, 1)
-let expected = item1
+let expected = true
 
 // Act
 testBasket.addItem(item1)
+actual = testBasket.items.includes(item1)
 
 // Assert
-actual = item1
-// expected = testBasket.getItems()[0]
-console.log(actual)
-console.log(expected)
 
 result = assertEquals(actual, expected)
 
 // Report
-console.log(`Item added to basket:`);
-console.log(`Test result: ${result}`);
+console.log(`Test 1 : Item is actually in basket:`);
+console.log(`Test result: ${result ? 'Pass' : 'Fail'}`);
+
+// Clean up
+afterEach()
+
+//! END OF TEST 1
+
+//? TEST 2
+//* After adding item to basket, does array Items increase in length by 1
+
+// Arrange
+
+expected = testBasket.items.length + 1
+
+item1 = new Item('salmon', 5, 1)
+
+//Act
+
+testBasket.addItem(item1)
+actual = testBasket.items.length
+
+//Assert
+result = assertEquals(actual, expected)
+
+// Report
+console.log(`Test 2 : After adding item to basket, does array Items increase in length by 1:`)
+console.log(`Test result: ${result ? 'Pass' : 'Fail'}`);
+
+
+

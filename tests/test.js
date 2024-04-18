a01bb0347b71266a0210a5049578bfbfd0f5d1af
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-18 16:58:35 +0100

    feat: add test for decreasing capacity below current item count

--- tests/test.js
+++ tests/test.js
@@ -2,12 +2,14 @@ import testFullCapacity from "./full-capacity.test.js";
 import testIncreaseCapacity from "./increase-capacity.test.js";
 import testRemoveItem from "./remove-item.test.js";
 import testRemoveItemEmpty from "./remove-item-empty.test.js";
+import testDecreaseCapacityToOverflow from "./decrease-capacity-overflow.test.js";
 
 const tests = [
   testFullCapacity,
   testIncreaseCapacity,
   testRemoveItem,
-  testRemoveItemEmpty
+  testRemoveItemEmpty,
+  testDecreaseCapacityToOverflow
 ];
 
 tests.forEach((test, i) => {

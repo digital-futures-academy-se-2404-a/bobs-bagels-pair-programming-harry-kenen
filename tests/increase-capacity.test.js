1ef48b39f76dbac9932d49a552a8b129f8fb6473
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-18 15:01:08 +0100

    refactor: abstracted out createRandomItem func into own module

--- tests/increase-capacity.test.js
+++ tests/increase-capacity.test.js
@@ -1,29 +1,7 @@
 import Basket from "../src/basket.js";
-import Item from "../src/item.js";
+import createRandomItem from "../src/create-random-item.js";
 
 const testIncreaseCapacity = () => {
-  const createRandomItem = (id) => {
-    // A function for creating random bagels based on a preset of flavors and prices. It takes an ID as an argument.
-    const menu = [
-      {
-        "flavor": "chocolate",
-        "price": 1.79
-      },
-      {
-        "flavor": "plain",
-        "price": 1.29
-      },
-      {
-        "flavor": "cream",
-        "price": 1.59
-      }
-    ];
-    const menuItem = menu[Math.floor(Math.random() * 3)]; // Picks a random item from the menu.
-    const item = new Item(menuItem.flavor, menuItem.price, id);
-
-    return item;
-  }
-
   // Creates an inventory first so that all possible ID's are assigned from beginning.
   const inventory = new Array();
   for (let i = 0; i < 15; i++) {

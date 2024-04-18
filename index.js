d5bb9fbe0ff4aaf14652ee8571dd7e0a8cfca685
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-18 11:14:24 +0100

    ADD: classes to index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+class Basket {
+  constructor() {
+    this.items = new Set();
+    this.capacity = 5;
+    this.totalValue = getTotalValue();
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.add(item);
+  }
+
+  removeItem(item) {
+    this.items.delete(item);
+  }
+
+  getTotalValue() {
+    let total = 0;
+    for (const element of this.items) {
+      total += element.price;
+    }
+    return total;
+  }
+}
+
+class Item {
+  constructor(flavor, price, id) {
+    this.flavor = flavor;
+    this.price = price;
+    this.id = id;
+  }
+
+  getFlavor() {
+    return this.flavor;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+}
+

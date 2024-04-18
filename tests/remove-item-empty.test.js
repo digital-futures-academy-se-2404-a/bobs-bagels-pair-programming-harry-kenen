854974d6b68c6854e3e9f3961ef8f3c88225b381
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-18 16:27:05 +0100

    test: added remove-item-empty.test.js

---
+++ tests/remove-item-empty.test.js
@@ -0,0 +1,15 @@
+import Basket from "../src/basket.js";
+
+export default function removeItemEmpty() {
+  console.log("TEST: An item cannot be removed from an empty basket.");
+
+  const expectedLength = 0;
+
+  const basket = new Basket();
+  
+  basket.removeItem(0);
+
+  const actualLength = basket.items.length;
+
+  expectedLength === actualLength ? console.log("PASS") : console.log(`FAIL: Expected ${expectedLength}, actual ${actualLength}.`);
+}
